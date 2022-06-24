fad58c059bd6fc758dda9d641e9cee78060dab9d
LauraMayock <mcfadden.laura@yahoo.com>
2022-06-24 20:31:32 +0000

    Add questions and startGame function

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// connect the HTML elements to javascript using variables
+const startButton = document.getElementById('start-btn')
+
+
+
+startButton.addEventListener("click",startGame)
+
+
+function startGame() {
+startButton.classList.add("hide")
+
+}
